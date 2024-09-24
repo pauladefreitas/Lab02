bdf9e59285562b753c4207b648c64218faa734e6
Paula de Freitas <113747605+pauladefreitas@users.noreply.github.com>
2024-09-23 22:55:39 -0300

    teste upload

--- Implementação/carjapa/src/main/java/com/locadora/carjapa/services/UserService.java
+++ Implementação/carjapa/src/main/java/com/locadora/carjapa/services/UserService.java
@@ -6,12 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.locadora.carjapa.models.User;
+import com.locadora.carjapa.models.dto.UserCreateDTO;
+import com.locadora.carjapa.models.dto.UserUpdateDTO;
 import com.locadora.carjapa.repositories.UserRepository;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 
 @Service
 public class UserService {
+
     @Autowired
     private UserRepository userRepository;
 
@@ -33,7 +37,6 @@ public class UserService {
     public User update(User obj) {
         User newObj = findById(obj.getId());
         newObj.setPassword(obj.getPassword());
-        newObj.setPassword(obj.getPassword());
         return this.userRepository.save(newObj);
     }
 
@@ -44,5 +47,19 @@ public class UserService {
         } catch (Exception e) {
             throw new RuntimeException("Não é possível excluir pois há entidades relacionadas!");
         }
+    }
+
+    public User fromDTO(@Valid UserCreateDTO obj) {
+        User user = new User();
+        user.setUsername(obj.getUsername());
+        user.setPassword(obj.getPassword());
+        return user;
+    }
+
+    public User fromDTO(@Valid UserUpdateDTO obj) {
+        User user = new User();
+        user.setId(obj.getId());
+        user.setPassword(obj.getPassword());
+        return user;
     }
 }
